ee8c953372e9147a4540ee7a3a6599b1034b5a20
shweta <dass10@myunitec.ac.nz>
2020-11-14 22:23:20 +1300

    update

---
+++ js/script.js
@@ -0,0 +1,189 @@
+BaseURL = "https://nzwetland.herokuapp.com/";
+
+function login(username, password){
+    $.ajax({
+        type: 'POST',
+        dataType: 'JSON',
+        url: BaseURL+'api/auth/login',
+        data: {username : username, password: password},
+        async: false,
+        success: function (data) {
+            localStorage.setItem('user', JSON.stringify(data.user));
+            localStorage.setItem('token', data.token);
+            console.log(JSON.stringify(data.user));
+            console.log(data.token);
+            location.reload();
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
+
+function hasValidToken(){
+    token = localStorage.getItem('token');
+    var loginStatus = false;
+    $.ajax({
+        type: 'GET',
+        dataType: 'JSON',
+        url: BaseURL+'api/auth/user',
+        headers: { 'Authorization': 'Token ' + token },
+        async: false,
+        success: function (data) {
+            console.log(data);
+            // alert('valid login');
+            loginStatus = true;
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+    return loginStatus;
+}
+
+function hasValidLogin(){
+    var loginStatus = false;
+    try {
+        user = JSON.parse(localStorage.getItem('user'));
+        token = localStorage.getItem('token');
+        // console.log("validToken: "+hasValidToken());
+        if (hasValidToken()){
+            loginStatus = true;
+        }
+    }
+    catch(err) {
+        console.log("no user login");
+    }
+    return loginStatus;
+}
+
+function logout(){
+    $.ajax({
+        type: 'POST',
+        dataType: 'JSON',
+        url: BaseURL+'api/auth/logout',
+        headers: { 'Authorization': 'Token ' + token },
+        async: false,
+        success: function (data) {
+            console.log(data);
+            localStorage.clear();
+            location.reload();
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
+
+
+
+/**
+ * @name getClientInfo
+ * @param clientID
+ * @returns client object
+ */
+function getClientInfo(clientID){
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async:false,
+        url: 'https://bigeye6.herokuapp.com/api/clients/'+clientID+'/',
+        success: function (data) {
+            // location.reload();
+            client = data;
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+    return client;
+
+}
+
+/**
+ * @name getSiteInfo
+ * @param site_id
+ * @returns site object
+ */
+function getSiteInfo (site_id) {
+    site = '';
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async: false,
+        url: 'https://nzwetland.herokuapp.com/api/site/'+site_id,
+        success: function (data) {
+            // location.reload();
+            site = data;
+
+            // var departmentID = data.department;
+
+            console.log(data);
+
+        },
+
+        error: function (err) {
+            console.log(err);
+        }
+    });
+    return site;
+}
+
+function getAllSites() {
+    sites = [];
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async:false,
+        url: 'https://nzwetland.herokuapp.com/api/site/',
+        success: function (data) {
+            sites = data;
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+    return sites;
+}
+
+$(document).ready(function () {
+    $("#login").hide();
+    $("#loggedin").hide();
+    $("#main").empty();
+    // console.log("valid login" + hasValidLogin());
+    if (hasValidLogin()) {
+        $("#login").hide();
+        $("#loggedin").show();
+        user = JSON.parse(localStorage.getItem('user'));
+        // check user group
+        groups = user.groups;
+        console.log(groups);
+        if (groups.includes(2)){
+            $("#main2").load("surveyofficermenu.html");
+        }
+        else if (groups.includes(1)){
+            $("#main").load("mainmenu.html");
+        }
+        else if (groups.includes(3)){
+            $("#main").load("dylan.html");
+        }
+
+
+    } else {
+        $("#login").show();
+        $("#loggedin").hide();
+
+    }
+
+    $("#loginBut").click(function () {
+        username = $("#username").val();
+        password = $("#password").val();
+        login(username, password);
+    });
+
+    $("#logoutBut").click(function (){
+        logout();
+
+    });
+});
+
---
+++ nzwetland1-origin-SHWETA/js/getsinglescript.js
@@ -0,0 +1,244 @@
+function getSinglelanduse(landuseID) {
+
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async: false,
+        url: 'http://nzwetland.herokuapp.com/api/landuse/' + landuseID + '/',
+        success: function (data) {
+            // location.reload();
+            landuse1 = data;
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+
+    });
+    return landuse1;
+}
+//HABITAT//
+function getSinglehabitat(habitatID) {
+
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async: false,
+        url: 'http://nzwetland.herokuapp.com/api/habitat/' + habitatID + '/',
+        success: function (data) {
+            // location.reload();
+            habitat1 = data;
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+
+    });
+    return habitat1;
+}
+
+//Site Habitat//
+//HABITAT//
+function getSingleSitehabitat(SitehabitatID) {
+
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async: false,
+        url: 'http://nzwetland.herokuapp.com/api/Sitehabitat/' + SitehabitatID + '/',
+        success: function (data) {
+            // location.reload();
+            Sitehabitat1 = data;
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+
+    });
+    return Sitehabitat1;
+}
+//SITE//
+function getSingleSite(siteID){
+    //return case object
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async:false,
+        url: 'https://nzwetland.herokuapp.com/api/site/'+siteID+'/',
+        success: function (data) {
+            // location.reload();
+            site1 = data;
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+
+    });
+    return site1;
+}
+
+function getSinglehabiatatLanduse(habiatalanduseid){
+    //return case object
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async:false,
+        url: 'http://nzwetland.herokuapp.com/api/sitehabitatlanduse/'+habiatalanduseid+'/',
+        success: function (data) {
+            // location.reload();
+            habitatlanduse1 = data;
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+
+    });
+    return habitatlanduse1;
+}
+
+
+
+function getAssignSitehabitatLanduse(sitehabitatID){
+    //return assessment objects
+    sitehabitatlanduses = [];
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async:false,
+        url: 'http://nzwetland.herokuapp.com/api/sitehabitatlanduse/',
+        success: function (data) {
+            // location.reload();
+            i = 0;
+            while (i<data.length){
+                sitehabitatlanduse = data[i];
+                if (sitehabitatlanduse.sitehabitat == sitehabitatID){
+                    sitehabitatlanduses.push(sitehabitatlanduse);
+                }
+                i=i+1;
+            }
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+
+    });
+    return sitehabitatlanduses;
+}
+
+function getsingleLanduse1(landuse){
+    //return assessment objects
+    landuses = [];
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async:false,
+        url: 'http://nzwetland.herokuapp.com/api/landuse/',
+        success: function (data) {
+            // location.reload();
+            i = 0;
+            while (i<data.length){
+             landuse = data[i];
+                if (landuse.id == landuse){
+                    landuses.push(landuse);
+                }
+                i=i+1;
+            }
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+
+    });
+    return landuses;
+}
+/*function getsitehabitat1(siteHabitat){
+    //return assessment objects
+    siteHabitats = [];
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async:false,
+        url: 'http://nzwetland.herokuapp.com/api/Sitehabitat/',
+        success: function (data) {
+            // location.reload();
+            i = 0;
+            while (i<data.length){
+                siteHabitat1 = data[i];
+                if (siteHabitat1.id == siteHabitat){
+                    siteHabitats.push(siteHabitat1);
+                }
+                i=i+1;
+            }
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+
+    });
+    return siteHabitats;
+}*/
+
+function getSitehabitat(sitHabitatID){
+    siteHabitat = "";
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async:false,
+        url: 'http://nzwetland.herokuapp.com/api/Sitehabitat/'+sitHabitatID+'/',
+        success: function (data) {
+            // location.reload();
+            siteHabitat = data
+            },
+
+        error: function (err) {
+            console.log(err);
+        }
+
+    });
+    return siteHabitat;
+}
+function getSingleSitehabitatlanduse(sitehabitatlanduseID){
+    //return case object
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async:false,
+        url: 'https://nzwetland.herokuapp.com/api/sitehabitatlanduse/'+sitehabitatlanduseID+'/',
+        success: function (data) {
+            // location.reload();
+            sitehabitatlanduse = data;
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+
+    });
+    return sitehabitatlanduse;
+}
+function getSingleSiteHabitat(siteHabitatID){
+    //return case object
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async:false,
+        url: 'https://nzwetland.herokuapp.com/api/Sitehabitat/'+siteHabitatID+'/',
+        success: function (data) {
+            // location.reload();
+            siteHabitat1 = data;
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+
+    });
+    return siteHabitat1;
+}
